5e0e56f8a24566de27d9d6b918821d4296020e86
Koji <yourpartner2021@gmail.com>
2025-02-15 16:29:18 +0900

    Initial Commit

---
+++ WeatherAndMap/WeatherAndMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAndMapApp.swift
+//  WeatherAndMap
+//
+//  Created by 田中康志 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAndMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
